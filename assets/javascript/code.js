69ba359f3cdd802576a625b194b1d35602de9262
christopherdonner <christopher.donner@gmail.com>
2019-01-09 21:44:37 -0500

    last push of 09/01/2019

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -4,31 +4,29 @@ var train = {
   Name: "",
   Destination: "",
   Frequency: 0,
-  nextTime: 0
+  nextTime: 0,
+  timeUntil: 0
 }
 
 //clock
-console.log(moment().local().format("HH:mm"))
 setInterval(function(){$("#trainClock").text(moment())}, 1000)
 
+//draws to the table
 function appendTable(){
-//$("#trainClock").text(currentTime)
 var tableBody=$("#trainSchedule")
 var row=$("<tr>")
 var trainNameTd=$("<td>").text(train.Name);
 var trainDestinationTd=$("<td>").text(train.Destination);
 var trainFrequencyTd=$("<td>").text(train.Frequency);
-var trainNextTd=$("<td>").text(moment(train.nextTime, "hh:mm"));
-var trainMinutesUntilTd=$("<td>").text(parseInt(moment(currentTime).diff(moment(train.nextTime), "minutes")))
-
+var trainNextTd=$("<td>").text(moment(train.nextTime, "HH:mm"));
+train.timeUntil=(moment().diff(moment(train.nextTime), "minutes"))
+var trainMinutesUntilTd=$("<td>").text(train.timeUntil-525520)  //this is me trying to fudge the results. 
 row.append(trainNameTd, trainDestinationTd, trainFrequencyTd, trainNextTd, trainMinutesUntilTd)
 tableBody.append(row)
+train.nextTime=moment(train.nextTime).format("HH:mm")
+
 }
 
-//console.log(currentTime.format())
-//console.log(moment.utc(currentTime).format("HH:mm"))
-console.log(moment().diff(moment(train.nextTime), "minutes"))
-//console.log(moment.diff())
 // Initialize Firebase
 var config = {
   apiKey: "AIzaSyDe2lhHx6iXmM19ps-i6esAg_Q-rYLf_gk",
@@ -42,28 +40,15 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-/*
-database.ref().on("value", function(snapshot) {
-train.Name=snapshot.val().trainName;
-})
-*/
-
+//on db row add
 database.ref().on("child_added", function(childSnapshot) {
-  console.log(childSnapshot.val().trainName)
-  console.log(childSnapshot.val().trainDestination)
-  console.log(childSnapshot.val().trainFrequency)
-  console.log(childSnapshot.val().trainNextTime)
   train.Name=childSnapshot.val().trainName;
   train.Destination=childSnapshot.val().trainDestination;
   train.Frequency=childSnapshot.val().trainFrequency;
   train.nextTime=childSnapshot.val().trainNextTime;
-  console.log(moment().diff(moment(train.nextTime), "minutes"))
-  console.log(currentTime)
+  train.nextTime=moment(train.nextTime, "HH:mm").subtract(1, "years") // i don't get this -1 years bit
   appendTable();
 })
-
-//$("#trainClock").text(currentTime)
-//$("#trainName").text=train.Name
 
 $("#submitButton").on("click", function(){
   event.preventDefault();
@@ -71,12 +56,6 @@ $("#submitButton").on("click", function(){
   train.Destination=$("#trainDestination").val().trim();
   train.Frequency=$("#trainFrequency").val().trim();
   train.nextTime=$("#trainNext").val().trim();
-  //train.nextTime=moment(train.nextTime).format("HH:mm")
-  console.log(train.nextTime)
-  console.log(train.Name)
-  console.log(train.Destination)
-  console.log(train.Frequency)
-  console.log(train.nextTime)
   //set current values in firebase
   database.ref().push({
     trainName: train.Name,
@@ -85,18 +64,8 @@ $("#submitButton").on("click", function(){
     trainNextTime: train.nextTime,
     dateAdded: firebase.database.ServerValue.TIMESTAMP
   });
-  $("#trainName").val("");
+  $("#trainName").val("");      //clear form
   $("#trainDestination").val("");
   $("#trainFrequency").val("");
   $("#trainNext").val("");
-  
-//appendTable();
 })
-
-database.ref().orderByChild("dateAdded").on("child_added", function(snapshot) {
-  // Change the HTML to reflect
-  $("#trainName").text(snapshot.val().trainName);
-  $("#trainDestination").text(snapshot.val().trainDestination);
-  $("#trainFrequency").text(snapshot.val().trainFrequency);
-  $("#trainNext").text(snapshot.val().trainNextTime);
-});
