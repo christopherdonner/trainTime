899daeb6f77ee8fdf28dcb054f6d0fddff34a491
Christopher Donner <christopher.donner@gmail.com>
2019-01-10 14:30:40 -0500

    moment.diff working?

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -18,12 +18,18 @@ var row=$("<tr>")
 var trainNameTd=$("<td>").text(train.Name);
 var trainDestinationTd=$("<td>").text(train.Destination);
 var trainFrequencyTd=$("<td>").text(train.Frequency);
+train.nextTime
 var trainNextTd=$("<td>").text(moment(train.nextTime, "HH:mm"));
-train.timeUntil=(moment().diff(moment(train.nextTime), "minutes"))
-var trainMinutesUntilTd=$("<td>").text(train.timeUntil-525520)  //this is me trying to fudge the results. 
+//train.timeUntil=(moment().diff(moment(train.nextTime), "minutes"))
+var trainTimeEpoch=moment(train.nextTime).unix()
+console.log(`epoch train time: ${trainTimeEpoch}`)
+console.log(moment().unix())
+train.timeUntil=moment().unix()-trainTimeEpoch
+console.log(train.timeUntil/60)
+var trainMinutesUntilTd=$("<td>").text(Math.floor(train.timeUntil/60)-525600)  //this is me trying to fudge the results. 
 row.append(trainNameTd, trainDestinationTd, trainFrequencyTd, trainNextTd, trainMinutesUntilTd)
 tableBody.append(row)
-train.nextTime=moment(train.nextTime).format("HH:mm")
+//train.nextTime=moment(train.nextTime).format("HH:mm")
 
 }
 
