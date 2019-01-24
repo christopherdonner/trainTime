4b0ab3333ffdb2df72319025068ef814a3c36e78
christopherdonner <christopher.donner@gmail.com>
2019-01-23 20:41:16 -0500

    final

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -17,11 +17,10 @@ var tableBody=$("#trainSchedule")
 var row=$("<tr>")
 var trainNameTd=$("<td>").text(train.Name);
 var trainDestinationTd=$("<td>").text(train.Destination);
-var trainFrequencyTd=$("<td>").text(train.Frequency);
 var trainNextTd=$("<td>").text(moment(train.nextTime, "HH:mm"));
 train.timeUntil=(moment().diff(moment(train.nextTime), "minutes"))
 var trainMinutesUntilTd=$("<td>").text(train.timeUntil-525520)  //this is me trying to fudge the results. 
-row.append(trainNameTd, trainDestinationTd, trainFrequencyTd, trainNextTd, trainMinutesUntilTd)
+row.append(trainNameTd, trainDestinationTd, trainNextTd, trainMinutesUntilTd)
 tableBody.append(row)
 train.nextTime=moment(train.nextTime).format("HH:mm")
 
@@ -69,3 +68,41 @@ $("#submitButton").on("click", function(){
   $("#trainFrequency").val("");
   $("#trainNext").val("");
 })
+
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+   type: 'bar',
+   data: {
+       labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+       datasets: [{
+           label: '# of Votes',
+           data: thermostat.tempArray,
+           backgroundColor: [
+               'rgba(255, 99, 132, 0.2)',
+               'rgba(54, 162, 235, 0.2)',
+               'rgba(255, 206, 86, 0.2)',
+               'rgba(75, 192, 192, 0.2)',
+               'rgba(153, 102, 255, 0.2)',
+               'rgba(255, 159, 64, 0.2)'
+           ],
+           borderColor: [
+               'rgba(255,99,132,1)',
+               'rgba(54, 162, 235, 1)',
+               'rgba(255, 206, 86, 1)',
+               'rgba(75, 192, 192, 1)',
+               'rgba(153, 102, 255, 1)',
+               'rgba(255, 159, 64, 1)'
+           ],
+           borderWidth: 1
+       }]
+   },
+   options: {
+       scales: {
+           yAxes: [{
+               ticks: {
+                   beginAtZero:true
+               }
+           }]
+       }
+   }
+});
