39702d64ce189f0eb9ca25fe708d32248d6435bd
Christopher Donner <christopher.donner@gmail.com>
2020-04-08 16:30:12 -0400

    modified some styling and added some input validation

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -49,12 +49,13 @@ database.ref().on("child_added", function(childSnapshot) {
   appendTable();
 })
 
+if($("#trainName").val()!=null && $("#trainDestination").val()!=null){
 $("#submitButton").on("click", function(){
   event.preventDefault();
   train.Name=$("#trainName").val().trim();
   train.Destination=$("#trainDestination").val().trim();
-  train.Frequency=$("#trainFrequency").val().trim();
-  train.nextTime=$("#trainNext").val().trim();
+  // train.Frequency=$("#trainFrequency").val().trim();
+  train.nextTime=$("#trainNext").val();
   //set current values in firebase
   database.ref().push({
     trainName: train.Name,
@@ -68,41 +69,4 @@ $("#submitButton").on("click", function(){
   $("#trainFrequency").val("");
   $("#trainNext").val("");
 })
-
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-   type: 'bar',
-   data: {
-       labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-       datasets: [{
-           label: '# of Votes',
-           data: thermostat.tempArray,
-           backgroundColor: [
-               'rgba(255, 99, 132, 0.2)',
-               'rgba(54, 162, 235, 0.2)',
-               'rgba(255, 206, 86, 0.2)',
-               'rgba(75, 192, 192, 0.2)',
-               'rgba(153, 102, 255, 0.2)',
-               'rgba(255, 159, 64, 0.2)'
-           ],
-           borderColor: [
-               'rgba(255,99,132,1)',
-               'rgba(54, 162, 235, 1)',
-               'rgba(255, 206, 86, 1)',
-               'rgba(75, 192, 192, 1)',
-               'rgba(153, 102, 255, 1)',
-               'rgba(255, 159, 64, 1)'
-           ],
-           borderWidth: 1
-       }]
-   },
-   options: {
-       scales: {
-           yAxes: [{
-               ticks: {
-                   beginAtZero:true
-               }
-           }]
-       }
-   }
-});
+}
